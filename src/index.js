e8d2e7ef1602ea10e3ebc137b7c22974861b381b
Junior Asosa <juniorassosa@gmail.com>
2024-04-30 16:20:38 +0100

    feat : allow cors for origin localhost:5173

--- src/index.js
+++ src/index.js
@@ -24,20 +24,20 @@ const {
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: [`http://localhost:${PORT}`, "*"],
+  origin: [`http://localhost:5173`],
 };
 dotenv.config();
 
 // Config
 app.use(json());
-app.use(cors());
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "x-access-token, Origin, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
 
 // Routes
 app.use(authBaseURI, authRouter);
